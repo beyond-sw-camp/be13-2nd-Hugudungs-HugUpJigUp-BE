bb0b7377ec6809fa01151be0ba3ee9ed700420d8
DESKTOP-HALTBK4\Playdata <yjw127588@gmail.com>
2025-03-04 11:55:20 +0900

    [유재우] 프로젝트 초기 설정

---
+++ src/main/java/com/hugudungs/hugupjigup/Config/SecurityConfig.java
@@ -0,0 +1,32 @@
+package com.hugudungs.hugupjigup.Config;
+
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.security.config.Customizer;
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
+import org.springframework.security.config.annotation.web.configurers.AbstractHttpConfigurer;
+import org.springframework.security.config.annotation.web.configurers.LogoutConfigurer;
+import org.springframework.security.web.SecurityFilterChain;
+
+@Configuration
+@EnableWebSecurity
+public class SecurityConfig {
+    @Bean
+    public SecurityFilterChain securityFilterChain(HttpSecurity http) throws Exception {
+        http.authorizeHttpRequests(authorize ->
+                        authorize.requestMatchers(
+                                "/api-docs/**",
+                                "/swagger-ui/**",
+                                "/api/v1/docs/swagger*/**",
+                                "/swagger-resources/**")
+                                    .permitAll()
+                                    .anyRequest()
+                                    .authenticated())
+                .logout(LogoutConfigurer::permitAll)
+                .csrf(AbstractHttpConfigurer::disable)
+                .cors(Customizer.withDefaults());
+
+        return http.build();
+    }
+}
---
+++ src/main/java/com/hugudungs/hugupjigup/Config/SwaggerConfig.java
@@ -0,0 +1,25 @@
+package com.hugudungs.hugupjigup.Config;
+
+import io.swagger.v3.oas.models.Components;
+import io.swagger.v3.oas.models.OpenAPI;
+import io.swagger.v3.oas.models.info.Info;
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+
+@Configuration
+public class SwaggerConfig {
+
+    @Bean
+    public OpenAPI openAPI(@Value("${springdoc.swagger-ui.version}") String springdocVersion) {
+        Info info = new Info()
+                .title("타이틀 입력")
+                .version(springdocVersion)
+                .description("API에 대한 설명 부분")
+                .version("1.0.0"); // API의 버전
+
+        return new OpenAPI()
+                .components(new Components())
+                .info(info);
+    }
+}
