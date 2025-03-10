7f31cb405339f73dd26807afa5637e7c38b5a533
DESKTOP-HALTBK4\Playdata <yjw127588@gmail.com>
2025-03-10 19:06:59 +0900

    [유재우] feature/entity/matching: BaseCommentEntity 네이밍 리팩토링 및 ApplicationQueue 엔티티 수정

--- src/main/java/com/hugudungs/hugupjigup/data/entity/comment/ApplicationQueue.java
+++ src/main/java/com/hugudungs/hugupjigup/data/entity/comment/ApplicationQueue.java
@@ -1,5 +1,6 @@
 package com.hugudungs.hugupjigup.data.entity.comment;
 
+import com.hugudungs.hugupjigup.data.entity.common.BaseEntity;
 import jakarta.persistence.AttributeOverride;
 import jakarta.persistence.AttributeOverrides;
 import jakarta.persistence.Column;
@@ -8,10 +9,9 @@ import jakarta.persistence.Table;
 
 @Entity
 @Table(name="application_queue")
-@AttributeOverrides({
-        @AttributeOverride(name = "content", column = @Column(name = "application_queue_content"))
-})
-public class ApplicationQueue extends BaseComment {
+public class ApplicationQueue extends BaseEntity {
+    @Column(nullable = false)
+    private String content;
 
     @Column(nullable = false)
     private Enum status; // 대기열 상태를 뜻함 (Ex. 대기중, 승낙, 멘토링 완료)
