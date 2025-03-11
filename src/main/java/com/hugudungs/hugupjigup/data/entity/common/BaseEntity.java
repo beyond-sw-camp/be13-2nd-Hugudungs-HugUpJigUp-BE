2ab416634ec2fd07be3737ffa4435549500f0667
DESKTOP-HALTBK4\Playdata <yjw127588@gmail.com>
2025-03-11 18:21:32 +0900

    [유재우]: BaseEntity의 id 값의 GenerationType을 Auto에서 IDENTITY로 변경(자동으로 id가 증가하지 않고 생성이 안되는 오류 해결)

--- src/main/java/com/hugudungs/hugupjigup/data/entity/common/BaseEntity.java
+++ src/main/java/com/hugudungs/hugupjigup/data/entity/common/BaseEntity.java
@@ -18,7 +18,7 @@ import java.time.LocalDateTime;
 @MappedSuperclass
 public abstract class BaseEntity {
     @Id
-    @GeneratedValue(strategy = GenerationType.AUTO)
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
     private long id;
 
     @Column(name = "created_at", updatable = false)
