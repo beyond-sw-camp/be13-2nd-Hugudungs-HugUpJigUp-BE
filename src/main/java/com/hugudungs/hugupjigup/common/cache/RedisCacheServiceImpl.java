9a5bff3a5dedac26880e1f081ffe4d0eda0f11b5
g00dbyul <skypower4203@gmail.com>
2025-03-11 20:22:25 +0900

    feat: init cache service

---
+++ src/main/java/com/hugudungs/hugupjigup/common/cache/RedisCacheServiceImpl.java
@@ -0,0 +1,33 @@
+package com.hugudungs.hugupjigup.common.cache;
+
+import lombok.RequiredArgsConstructor;
+import org.springframework.data.redis.core.RedisTemplate;
+import org.springframework.stereotype.Service;
+
+import java.util.concurrent.TimeUnit;
+
+@Service
+@RequiredArgsConstructor
+public class RedisCacheServiceImpl implements CacheService {
+    private final RedisTemplate<String, String> redisTemplate;
+
+    @Override
+    public String get(String key) {
+        return redisTemplate.opsForValue().get(key);
+    }
+
+    @Override
+    public void set(String key, String value, long seconds) {
+        redisTemplate.opsForValue().set(key, value, seconds, TimeUnit.SECONDS);
+    }
+
+    @Override
+    public void delete(String key) {
+        redisTemplate.delete(key);
+    }
+
+//    @Override
+//    public void deleteAl() {
+////        redisTemplate.delete();
+//    }
+}
