ecf189e5bc51f8a54c02fc2e8195d4dcb15bd8aa
ericyum <ericyum9196@gmail.com>
2025-03-12 20:57:01 +0900

    [염정운] 이전 커밋에 반영이 안되서 추가 커밋함.

--- src/main/java/com/hugudungs/hugupjigup/data/entity/board/Free.java
+++ src/main/java/com/hugudungs/hugupjigup/data/entity/board/Free.java
@@ -9,16 +9,13 @@ import java.util.Date;
 @Table(name = "free")
 @Getter
 @Setter
-@NoArgsConstructor
-@AllArgsConstructor
-@Builder
 @AttributeOverrides({
-        @AttributeOverride(name = "id", column = @Column(name = "free_text_id", nullable = false)),
+        @AttributeOverride(name = "id", column = @Column(name = "free_id", nullable = false)),
         @AttributeOverride(name = "title", column = @Column(name = "free_title", nullable = false)),
-        @AttributeOverride(name = "content", column = @Column(name = "free_text", nullable = false, columnDefinition = "TEXT")),
-        @AttributeOverride(name = "views", column = @Column(name = "free_number", nullable = false, columnDefinition = "INT DEFAULT 0"))
+        @AttributeOverride(name = "content", column = @Column(name = "free_content", nullable = false, columnDefinition = "TEXT")),
+        @AttributeOverride(name = "views", column = @Column(name = "free_views", nullable = false, columnDefinition = "INT DEFAULT 0"))
 })
-public class FreeText extends BaseBoardEntity{
+public class Free extends BaseBoardEntity{
     @ManyToOne
     @JoinColumn(name = "board_type_id", nullable = false)
     private BoardTypes boardTypeId;
@@ -26,8 +23,4 @@ public class FreeText extends BaseBoardEntity{
     @ManyToOne
     @JoinColumn(name = "user_id", nullable = false)
     private User userId;
-
-    @Column(name = "free_date", nullable = false)
-    @Temporal(TemporalType.DATE) // 자동으로 '년-월-일'형의 날짜 입력
-    private Date freeDate;
 }
--- src/main/java/com/hugudungs/hugupjigup/data/entity/comment/FreeComment.java
+++ src/main/java/com/hugudungs/hugupjigup/data/entity/comment/FreeComment.java
@@ -1,6 +1,6 @@
 package com.hugudungs.hugupjigup.data.entity.comment;
 
-import com.hugudungs.hugupjigup.data.entity.board.FreeText;
+import com.hugudungs.hugupjigup.data.entity.board.Free;
 import com.hugudungs.hugupjigup.data.entity.user.User;
 import jakarta.persistence.*;
 
@@ -11,18 +11,15 @@ import lombok.*;
 @Table(name = "free_comment")
 @Getter
 @Setter
-@NoArgsConstructor
-@AllArgsConstructor
-@Builder
 @AttributeOverrides({
-        @AttributeOverride(name = "id", column = @Column(name = "free_text_comment_id", nullable = false)),
-        @AttributeOverride(name = "content", column = @Column(name = "free_comment_text", nullable = false, columnDefinition = "TEXT"))
+        @AttributeOverride(name = "id", column = @Column(name = "free_comment_id", nullable = false)),
+        @AttributeOverride(name = "content", column = @Column(name = "free_comment_content", nullable = false, columnDefinition = "TEXT"))
 })
 public class FreeComment extends BaseCommentEntity {
 
     @ManyToOne
-    @JoinColumn(name = "free_text_id", nullable = false)
-    private FreeText freeText;
+    @JoinColumn(name = "free_id", nullable = false)
+    private Free free;
 
     @ManyToOne
     @JoinColumn(name = "user_id", nullable = false)
@@ -31,8 +28,4 @@ public class FreeComment extends BaseCommentEntity {
     @ManyToOne
     @JoinColumn(name = "comment_type_id", nullable = false)
     private CommentTypes commentTypeId;
-
-    @Column(name = "free_comment_date", nullable = false)
-    @Temporal(TemporalType.DATE) // 자동으로 '년-월-일'형의 날짜 입력
-    private Date freeCommentDate;
 }
