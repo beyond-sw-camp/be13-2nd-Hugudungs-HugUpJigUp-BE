0a9a44173a3842b684a5d3984847b9b61f217c22
ericyum <ericyum9196@gmail.com>
2025-03-21 05:02:24 +0900

    [염정운] 자유 게시글을 삭제 할 때 해당 글에 댓글이 있을 경우 연관 키로 인해 삭제 되지 않는 현상 수정

--- src/main/java/com/hugudungs/hugupjigup/data/entity/board/Free.java
+++ src/main/java/com/hugudungs/hugupjigup/data/entity/board/Free.java
@@ -1,14 +1,19 @@
 package com.hugudungs.hugupjigup.data.entity.board;
 
+import com.hugudungs.hugupjigup.data.entity.comment.FreeComment;
 import jakarta.persistence.AttributeOverride;
 import jakarta.persistence.AttributeOverrides;
+import jakarta.persistence.CascadeType;
 import jakarta.persistence.Column;
 import jakarta.persistence.Entity;
+import jakarta.persistence.OneToMany;
 import jakarta.persistence.Table;
 
 import lombok.Getter;
 import lombok.NoArgsConstructor;
 import lombok.experimental.SuperBuilder;
+
+import java.util.List;
 
 @Entity
 @SuperBuilder
@@ -23,5 +28,8 @@ import lombok.experimental.SuperBuilder;
 })
 
 
-public class Free extends BaseBoardEntity{ }
+public class Free extends BaseBoardEntity{
+    @OneToMany(mappedBy = "free", cascade = CascadeType.REMOVE, orphanRemoval = true) // ✅ 삭제 시 연관 댓글도 삭제
+    private List<FreeComment> comments;
+}
 
